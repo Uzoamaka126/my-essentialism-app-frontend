378187636bdbf7c6073a4cda3135937a3244fdfb
Uzoamaka126 <vanessapeters126@gmail.com>
2020-05-16 12:35:50 +0100

    Add prev and next components and buttons

--- src/ProtectedApp/components/Dashboard/Home/Onboarding/components/StepOne.js
+++ src/ProtectedApp/components/Dashboard/Home/Onboarding/components/StepOne.js
@@ -1,20 +1,15 @@
 import React from "react";
-import { Box, Text } from "@chakra-ui/core";
+import { Box, Flex } from "@chakra-ui/core";
+import { Wrapper, Title, SubTitle } from "./BoxWrapper";
 
 export function StepOne(props) {
   return (
-    <Box>
-      <Text fontSize="24px" fontWeight="semibold">
-        What does Essentialism do?
-      </Text>
-      <Text fontSize="20px" fontWeight="normal">
-        Essentialism helps you set principles you would like to have and work
-        towards achieveing them.
-      </Text>
-      <Text fontSize="14px" fontWeight="normal">
-        Like a to-do list, but for solely for self-development.
-      </Text>
-      
-    </Box>
+    <Wrapper>
+      <Title heading="What does Essentialism do?" />
+      <SubTitle
+        text="Essentialism helps you set principles you would like to have and work
+        towards achieveing them. Like a to-do list, but for solely for self-development."
+      />
+    </Wrapper>
   );
 }
