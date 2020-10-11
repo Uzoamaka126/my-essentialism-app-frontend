7090ee01f155d2fd3e396093cb808c372a2e0f4a
Uzoamaka126 <vanessapeters126@gmail.com>
2020-10-11 12:32:00 +0100

    add second section to landing page

--- src/PublicApp/Home_copy.js
+++ src/PublicApp/Home_copy.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { Box, Flex, Text, Image, Link, Button } from "@chakra-ui/core";
+import { Box, Flex, Text, Image, Link, Button, Stack } from "@chakra-ui/core";
 import BgImg from "../Components/assets/big-shoes-1.svg";
-import Park from "../Components/assets/progress.svg";
 import { Link as RouterLink } from "react-router-dom";
 import { getToken } from "../Utilities/localStorage";
 import Logo from "../Components/assets/Logo.svg";
+import Select from "../Components/assets/select_values.svg";
+import Create from "../Components/assets/create.svg";
+import Tick from "../Components/assets/Tick_Off.svg";
 
 function NavLink({ to, label }) {
   return (
@@ -26,6 +28,7 @@ function NavLink({ to, label }) {
     </Link>
   );
 }
+
 function Navbar(props) {
   const token = getToken();
 
@@ -61,47 +64,57 @@ function Navbar(props) {
   );
 }
 
+function ImageAndText({ src, label, alt, imageWidth }) {
+  return (
+    <Box>
+      <Box width={imageWidth} maxWidth="280px" height="160px" marginX="auto">
+        <Image width="100%" height="100%" src={src} alt={alt} />
+      </Box>
+      <Text marginTop="40px" fontSize="22px" color="#585858" textAlign="center">
+        {label}
+      </Text>
+    </Box>
+  );
+}
 export default function Home(props) {
   return (
     <Box width="100%" background="#fff">
       <Navbar />
-      <Flex
+      <Box
         background="#fff"
         height="auto"
-        paddingLeft="20px"
-        paddingRight="20px"
+        paddingX="20px"
         maxWidth="1400px"
-        paddingTop="100px"
+        paddingY="100px"
       >
-        <Flex
-          paddingLeft="20px"
-          paddingRight="20px"
-          paddingTop="1rem"
-          width="50%"
-          alignSelf="center"
-          flexDirection="column"
-        >
-          <Text
-            fontSize="50px"
-            color="#2D3335"
-            fontWeight="semibold"
-            lineHeight="61px"
+        <Flex>
+          <Flex
+            paddingLeft="20px"
+            paddingRight="20px"
+            paddingTop="1rem"
+            width="50%"
+            alignSelf="center"
+            flexDirection="column"
           >
-            Smart way to easily cultivate values
-          </Text>
-          <Text
-            lineHeight="35px"
-            paddingTop="0.625rem"
-            color="#585858"
-            fontSize="27px"
-            marginTop="0.8rem"
-          >
-            Put in time to improve the quality of your life
-          </Text>
+            <Text
+              fontSize="50px"
+              color="#2D3335"
+              fontWeight="semibold"
+              lineHeight="60px"
+            >
+              Smart way to easily cultivate values
+            </Text>
+            <Text
+              lineHeight="35px"
+              color="#585858"
+              fontSize="27px"
+              marginY="24px"
+            >
+              Put in time to improve the quality of your life
+            </Text>
             <Link
               as={RouterLink}
               to="/signup"
-              marginTop="2.5rem"
               bg="#fff"
               color="#2D7992"
               border="1px solid #2D7992"
@@ -113,14 +126,44 @@ export default function Home(props) {
             >
               Get Started
             </Link>
+          </Flex>
+          <Box width="45%" paddingTop="1rem">
+            <Image src={BgImg} alt="background" width="100%" height="100%" />
+          </Box>
         </Flex>
-        <Box
-          width="45%"
-          paddingTop="1rem"
+      </Box>
+      <Box paddingY="100px" bg="#F5FAFF">
+        <Text
+          fontSize="30px"
+          color="#585858"
+          fontWeight="semibold"
+          lineHeight="60px"
+          textAlign="center"
         >
-          <Image src={BgImg} alt="background" width="100%" height="100%" />
-        </Box>
-      </Flex>
+          How Essentialism Works
+        </Text>
+        <Stack isInline spacing={10} align="center" paddingX="50px"
+        marginTop="40px">
+          <ImageAndText
+            alt="woman selecting stuff"
+            src={Select}
+            imageWidth="280px"
+            label="Select areas of your life that you’d like to improve"
+          />
+          <ImageAndText
+            alt="woman selecting stuff"
+            src={Create}
+            imageWidth="250px"
+            label="Create projects and tasks to be done around those areas"
+          />
+          <ImageAndText
+            alt="woman selecting stuff"
+            src={Tick}
+            imageWidth="280px"
+            label="Tick off projects when you are done with them"
+          />
+        </Stack>
+      </Box>
     </Box>
   );
 }
